224655ee08155bc080bab07f92f85b0ade5d93d4
Timo Aho <timo@phz.fi>
2019-09-19 16:58:53 +0300

    Upgrade dependencies, deprecate yarn

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,4 @@
-import {configure} from '@kadira/storybook';
-import {setOptions} from '@kadira/storybook-addon-options';
+import { configure } from '@storybook/react';
 
-setOptions({
-  name: 'INFINITE CALENDAR',
-  url: 'https://github.com/clauderic/react-infinite-calendar',
-  goFullScreen: false,
-  showLeftPanel: true,
-  showDownPanel: false,
-  showSearchBox: false,
-  downPanelInRight: false,
-  sortStoriesByKind: false,
-});
-
-configure(() => require('../src/.stories/index.js'), module);
+// automatically import all files ending in *.stories.js
+configure(require.context('../src/.stories', true, /\.js$/), module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,59 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+const path = require('path')
+
 module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
   module: {
-    loaders: [
+    rules: [
+
+
       {
-        test: /(\.scss)$/,
-        loaders: [
-          'style',
-          'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]',
-          'sass?sourceMap'
-        ]
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          require.resolve('sass-loader'),
+          {
+            loader: require.resolve("postcss-loader"),
+            options: {
+              ident: "postcss",
+              plugins: () => [
+                require("postcss-flexbugs-fixes"),
+                require("postcss-inline-svg"),
+                require("postcss-svgo")
+                /*
+                autoprefixer({
+                  browsers: [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "not ie < 9"
+                  ],
+                  flexbox: "no-2009"
+                })
+                */
+              ]
+            }
+          }
+        ],
+        include: path.resolve(__dirname, "../")
       }
-    ]
-  }
+    ],
+  },
 };
