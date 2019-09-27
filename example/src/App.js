9227ee86a6002fc29cedd4edfa8117a0c64e1d11
Timo Aho <timo.aho@phz.fi>
2019-09-27 20:46:38 +0300

    add working example

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import InfiniteCalendar from 'react-infinite-calendar'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <InfiniteCalendar />
+      </div>
+    )
+  }
+}
